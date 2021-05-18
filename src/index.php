360bcbd37af5ca5e59ad61e0e8b2eca3d10236a1
YOUR_USERNAME <emaili@musk.com>
2021-05-18 17:46:02 +0200

    zmeny podla commentu

--- src/index.php
+++ src/index.php
@@ -22,8 +22,7 @@ $puzzleHelper = new PuzzleHelper();
 
 $generator = new BacktrackGenerator($puzzleHelper);
 
-
-		$puzzle = new Puzzle($generator, $puzzleHelper);
+$puzzle = new Puzzle($generator, $puzzleHelper);
 
 $application = new Application($viewer, $puzzle);
 
