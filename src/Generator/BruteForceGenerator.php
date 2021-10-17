00ea6d148f6708c87262684e927668de298bd978
Pavol Senko <pavol.senko@gmail.com>
2021-10-17 15:59:40 +0200

    implement brute force generator

--- src/Generator/BruteForceGenerator.php
+++ src/Generator/BruteForceGenerator.php
@@ -3,12 +3,57 @@ declare(strict_types = 1);
 
 namespace SudokuPhp\Generator;
 
+use SudokuPhp\Helper\PuzzleHelper;
 use SudokuPhp\Puzzle\SudokuGrid;
 
 class BruteForceGenerator implements GeneratorInterface
 {
+    private PuzzleHelper $puzzleHelper;
+
+    public function __construct(PuzzleHelper $puzzleHelper)
+    {
+        $this->puzzleHelper = $puzzleHelper;
+        $this->sudokuGrid = new SudokuGrid($this->puzzleHelper->createEmptyGrid());
+    }
+
+    /**
+     * Bruteforce a Sudoku puzzle
+     */
     public function generate(): SudokuGrid
     {
-        return new SudokuGrid();
+        $this->sudokuGrid = $this->sudokuGrid->setGridRow(0, $this->puzzleHelper->generateRandomNiner());
+
+        $iterations = 0;
+        $row = 1;
+
+        while ($row < 9) {
+            $iterations++;
+
+            $nextRow = $this->puzzleHelper->generateRandomNiner();
+
+            $isValidRow = true;
+            for ($i = 0; $i < 9; $i++) {
+                $isValidNumber = $this->puzzleHelper->isValidNumber(
+                    $nextRow[$i],
+                    $this->sudokuGrid->getGrid(),
+                    $row,
+                    $i,
+                );
+
+                if (!$isValidNumber) {
+                    $isValidRow = false;
+                    break;
+                }
+            }
+
+            if ($isValidRow) {
+                $this->sudokuGrid = $this->sudokuGrid->setGridRow($row, $nextRow);
+                $row++;
+            }
+        }
+
+        $this->sudokuGrid->setNote('Number of iteration to generate (bruteforce): ' . $iterations);
+
+        return $this->sudokuGrid;
     }
 }
